0d5f699693f601c26409b3b6bde87a41c8420083
Wei Jung Huang <whuang602@csilvm-17.cs.ucsb.edu>
2023-10-17 22:32:34 -0700

    wh - almost done 24/60 on gradescope

--- MP1/mp1.py
+++ MP1/mp1.py
@@ -27,18 +27,12 @@ def run_train_test(training_input, testing_input):
                 "precision": #your_precision
             }
     """
-
-    results = {"tpr":0,"fpr":0,"error_rate":0,"accuracy":0,"precision":0}
     
     d = training_input[0][0]
-    n_lst = []
-    for i in training_input[0][1:]:
-        n_lst.append(i)
+    n_lst = [i for i in training_input[0][1:]]
     
     # [A,B,C] or potentially more
-    centroids = []
-    for i in range(len(n_lst)):
-        centroids.append(np.array([0.0,0.0,0.0])) # dependent on var d
+    centroids = [np.array([0.0 for x in range(d)]) for i in range(len(n_lst))] # dependent on var d
         
     i = 1 # temp var, end point for each class, accounts for the number of data in each class.
     # Ex. skip 1, up til but excl. 501, up til but excl. 1001, up til but excl. 1501
@@ -50,7 +44,7 @@ def run_train_test(training_input, testing_input):
     print("centroids =", centroids)
     
     # [A/B, B/C, C/A]
-    vector_slope = [centroids[0]-centroids[1],centroids[1]-centroids[2],centroids[2]-centroids[0]]
+    vector_slope = [centroids[0]-centroids[1],centroids[1]-centroids[2],centroids[0]-centroids[2]]
     print("slope =",vector_slope)
     
     # Halfway between each pair of centroids
@@ -85,34 +79,107 @@ def run_train_test(training_input, testing_input):
     for i in range(len(n_lst)):
         # equivalent to vector_slope[index][0] * centroids[index][0] + vector_slope[index][1] * centroids[index][1] + vector_slope[index][2] * centroids[index][2] - d
         positive_side.append(true_class[i]) if (np.sum(vector_slope[i] * centroids[i]) - d_offsets[i]) > 0 else positive_side.append(other_class[i])
-    print(positive_side)
+    # print(positive_side)
     
     # --------------------------------------- Testing ---------------------------------------
     
     
     d = testing_input[0][0]
-    n_lst = []
-    for i in testing_input[0][1:]:
-        n_lst.append(i)
+    n_lst = [ i for i in testing_input[0][1:] ]
     
     i, index = 1, 0
-    margin = [[0,0],[0,0],[0,0]]
+    # vector_slope: [ A/B , B/C, C/A]
+    final_results = []
     for clss in range(len(n_lst)):
         for line in testing_input[i:i+n_lst[clss]]:
-            if (np.sum(vector_slope[index] * line) - d_offsets[index]) > 0:
-                if positive_side[index] == true_class[index]:
-                    margin[index][0]+=1
-                else:
-                    margin[index][1]+=1
-            else:
-                if positive_side[index] == true_class[index]:
-                    margin[index][1] +=1
-                else:
-                    margin[index][0] +=1
+            curr_index = 0
+            calc = np.sum(vector_slope[curr_index] * line) - d_offsets[curr_index]
+            # curr_index = 2 if (calc > 0 and positive_side[curr_index] == "A") or (calc < 0 and positive_side[curr_index] != "A") else 1
+            if calc > 0: # positive side of A/B
+                if positive_side[0] == "A": # point is A, now check A or C
+                    calc = np.sum(vector_slope[2] * line) - d_offsets[2]
+                    final_results.append("A") if ( calc >= 0 and positive_side[2] =="A" ) or (calc < 0 and positive_side[2] != "A") else final_results.append("C")
+                else: # point is B, now check B or C
+                    calc = np.sum(vector_slope[1] * line) - d_offsets[1]
+                    final_results.append("B") if ( calc >= 0 and positive_side[1] =="B" ) or (calc < 0 and positive_side[1] != "B") else final_results.append("C")
+                continue      
+            if calc < 0: # negative side of A/B
+                if positive_side[0] == "A": # point is B
+                    calc = np.sum(vector_slope[1] * line) - d_offsets[1]
+                    final_results.append("B") if ( calc >= 0 and positive_side[1] =="B" ) or (calc < 0 and positive_side[1] != "B") else final_results.append("C")
+                else: # point is A
+                    calc = np.sum(vector_slope[2] * line) - d_offsets[2]
+                    final_results.append("A") if ( calc >= 0 and positive_side[2] =="A" ) or (calc < 0 and positive_side[2] != "A") else final_results.append("C")
+                continue
+            # on the A/B discrimnant function plane (tie), prefer A -> test A or C 
+            calc = np.sum(vector_slope[2] * line) - d_offsets[2]
+            final_results.append("A") if ( calc >= 0 and positive_side[2] =="A" ) or (calc < 0 and positive_side[2] != "A") else final_results.append("C")
         i+=n_lst[clss]
         index+=1
-    print(margin)
     
+    print(final_results)
     
-        
+    confusion_matrix = [[0,0,0] for x in range(3)]
+    convenience = {"A":0, "B":1, "C":2}
+    i, index = 1, 0
+    for clss in range(len(n_lst)):
+        for result in final_results[i:i+n_lst[clss]]:
+            confusion_matrix[convenience[result]][convenience[true_class[index]]]+=1 # imagine writing nested switch statements, yikes
+        i+=n_lst[clss]
+        index+=1
+    for i in range(3):
+        print(confusion_matrix[i])
+    
+    # [True Positive, False Positive, False Negative, True Negative]
+    confusion_matrix_specific = [[0.0,0.0,0.0,0.0] for x in range(3)]
+    
+    # Class A
+    confusion_matrix_specific[0][0]+=confusion_matrix[0][0]
+    confusion_matrix_specific[0][1]+=confusion_matrix[0][1]+confusion_matrix[0][2]
+    confusion_matrix_specific[0][2]+=confusion_matrix[1][0]+confusion_matrix[2][0]
+    confusion_matrix_specific[0][3]+=confusion_matrix[1][1]+confusion_matrix[1][2]+confusion_matrix[2][1]+confusion_matrix[2][2]
+    
+    # Class B
+    confusion_matrix_specific[1][0]+=confusion_matrix[1][1]
+    confusion_matrix_specific[1][1]+=confusion_matrix[1][0]+confusion_matrix[1][2]
+    confusion_matrix_specific[1][2]+=confusion_matrix[0][1]+confusion_matrix[2][1]
+    confusion_matrix_specific[1][3]+=confusion_matrix[0][0]+confusion_matrix[0][2]+confusion_matrix[2][0]+confusion_matrix[2][2]
+    
+    # Class C
+    confusion_matrix_specific[2][0]+=confusion_matrix[2][2]
+    confusion_matrix_specific[2][1]+=confusion_matrix[2][0]+confusion_matrix[2][1]
+    confusion_matrix_specific[2][2]+=confusion_matrix[0][2]+confusion_matrix[1][2]
+    confusion_matrix_specific[2][3]+=confusion_matrix[0][0]+confusion_matrix[0][1]+confusion_matrix[1][0]+confusion_matrix[1][1]
+    print(confusion_matrix_specific)
+            
+    pre_average_results = [ np.array([0.0,0.0,0.0,0.0,0.0]) for x in range(3)]
+    
+    index=0
+    for clss in confusion_matrix_specific:
+        pre_average_results[index][0] = clss[0] / (clss[0] + clss[2])
+        pre_average_results[index][1] = clss[1] / (clss[1] + clss[3])
+        pre_average_results[index][2] = (clss[1] + clss[2]) / np.sum(clss)
+        pre_average_results[index][3] = (clss[0] + clss[3]) / np.sum(clss)
+        pre_average_results[index][4] = clss[0] / (clss[0] + clss[1])
+        index+=1
+    
+    # tpr = TP / P where P = TP + FN
+    # fpr = FP / N where N = FP + TN
+    # error rate = (FP + FN) / (P+N)
+    # accuracy = (TP+TN)/(P+N)
+    # Precision = TP / P_hat where P_hat = TP + FP
+    # [TP, FP, FN, TN]
+    results = {"tpr": (pre_average_results[0][0]+pre_average_results[1][0]+pre_average_results[2][0])/3,
+               "fpr": (pre_average_results[0][1]+pre_average_results[1][1]+pre_average_results[2][1])/3,
+               "error_rate": (pre_average_results[0][2]+pre_average_results[1][2]+pre_average_results[2][2])/3,
+               "accuracy": (pre_average_results[0][3]+pre_average_results[1][3]+pre_average_results[2][3])/3,
+               "precision": (pre_average_results[0][4]+pre_average_results[1][4]+pre_average_results[2][4])/3
+               }
+    # results = {"tpr": 0.8933333333333334,
+    #            "fpr": 0.05333333333333334,
+    #            "error_rate": 0.07111111111111111,
+    #            "accuracy": 0.9288888888888889,
+    #            "precision": 0.8923242082662372}
+    print(results)
+    
     return results
